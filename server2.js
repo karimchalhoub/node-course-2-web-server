2ae5cc2a23ff5c3393ef3c1d64a22081b7d3c765
karimchalhoub <karim.chalhoub@gmail.com>
2017-09-02 17:41:41 -0700

    Second run commit

---
+++ server2.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials'); // for headers and footers
+app.set('view engine', 'hbs'); // to use handlbars as a view engine
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    console.log('unable to append server.log');
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs', {
+    pageTitle: 'Maintenance Page',
+    maintMessage: 'Site under maintenance'
+  });
+});
+
+app.use(express.static(__dirname + '/public')); //to set a static directory for public files
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  //res.send('<h1>Hello Express</h1>');
+  res.render('home2.hbs', {
+    pageTitle: 'Home Page',
+    // currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about2.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errMessage: 'Error handling request'
+  });
+});
+
+app.listen(3000, ()=> {
+  console.log('server is up on port 3000');
+});
